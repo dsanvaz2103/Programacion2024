2ad4a2094e0eba94f32e90459436f04a74bc13c5
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2024-10-28 12:36:42 +0100

    Actividad 8 entregada

---
+++ NetBeansProjects/Ejercicio8/src/Ejercicio8/Ejercicio8.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author David Sánchez Vázquez
+ */
+public class Ejercicio8 {
+    public static void main(String[] args){
+        System.out.println(3<= 5 && 2 == 2);
+        System.out.println(3 <=5 && 2 > 10);
+        System.out.println(1 != 2 || 5 < 3);
+        System.out.println(!(1 < 2));
+    }
+}
