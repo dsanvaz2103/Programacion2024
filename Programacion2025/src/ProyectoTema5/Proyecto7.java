46563c68347385e90c17831de43bccfe514f4494
dsanvaz2103 <151721658+dsanvaz2103@users.noreply.github.com>
2025-04-24 13:04:11 +0200

    Actualizacion

--- Programacion2025/src/ProyectoTema5/Proyecto7.java
+++ Programacion2025/src/ProyectoTema5/Proyecto7.java
@@ -2,28 +2,46 @@ package ProyectoTema5;
 
 import ProyectoTema5.utils.Juego;
 import ProyectoTema5.utils.Juegos;
+import ProyectoTema5.utils.Juegos2;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Proyecto7 {
     public static void main(String[] args) {
-        List<Juego> juegos = new ArrayList<>();
-        Juego juego = new Juego("Dark Souls",Juegos.ACCION,10000,9.5);
-        Juego juego3 = new Juego("GTA5",Juegos.ACCION,10000,7.5);
-        Juego juego1 = new Juego("Minecraft",Juegos.AVENTURA,10000,2.5);
-        Juego juego2 = new Juego("Asseto",Juegos.SIMULACION,10000,8);
-        juegos.add(juego);
+        TreeSet<Juego> juegos  = new TreeSet<>();
+
+        // Crear juegos existentes
+        Juego juego1 = new Juego("Dark Souls", Juegos.ACCION, 10000, 9.5);
+        Juego juego2 = new Juego("GTA5", Juegos.ACCION, 10000, 7.5);
+        Juego juego3 = new Juego("Minecraft", Juegos.AVENTURA, 10000, 2.5);
+        Juego juego4 = new Juego("Assetto Corsa", Juegos.SIMULACION, 10000, 8);
+        Juego juego5 = new Juego("Tetris", Juegos.PUZZLE, 10000, 8.9);
+
+        // Nuevos juegos añadidos
+        Juego juego6 = new Juego("FIFA 23", Juegos.DEPORTES, 15000, 8.2);
+        Juego juego7 = new Juego("Age of Empires IV", Juegos.ESTRATEGIA, 5000, 9.0);
+        Juego juego8 = new Juego("Fortnite", Juegos.ACCION, 30000, 8.4);
+        Juego juego9 = new Juego("The Witcher 3", Juegos.AVENTURA, 12000, 9.7);
+        Juego juego10 = new Juego("Rocket League", Juegos.DEPORTES, 8000, 8.8);
+
+        // Añadir juegos a la lista
         juegos.add(juego1);
         juegos.add(juego2);
         juegos.add(juego3);
-
+        juegos.add(juego4);
+        juegos.add(juego5);
+        juegos.add(juego6);
+        juegos.add(juego7);
+        juegos.add(juego8);
+        juegos.add(juego9);
+        juegos.add(juego10);
         System.out.println("\nRanking ordenado por Puntuacion Media");
-        for(Juego p: juegos) {
-            System.out.println(p);
+        System.out.println("\n Inverso \n");
+        Iterator<Juego> iterator = juegos.descendingIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
-
     }
 }
